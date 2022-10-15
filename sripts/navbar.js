4699e2ff5df4334ab72d53bbcffc325d8836ef06
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2022-10-15 22:37:22 +0530

    css update, animations

---
+++ sripts/navbar.js
@@ -0,0 +1,22 @@
+var navState = false;
+
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+    navState=true;
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+    navState=false;
+  }
+
+  function toogleNav() {
+    if(navState==false) {
+        openNav(); 
+        navState=true;
+    }
+    else{
+        closeNav();
+        navState=false;
+    }
+  }
