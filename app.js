effeef079bf1c5a3de5c8b87a0daf71de9484f8f
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2022-10-19 07:20:36 +0530

    added verified mark

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const app = Vue.createApp({
             informative: "20%",
             hatefull: "60%",
             check: "check",
-            verified: "developer"
+            verified: "developer",
+            instagram: "https://www.instagram.com/akshay._.vs__",
+            twitter: "https://twitter.com/Akshay_vs__",
+            snapchat: ""
         }
     },
     methods: {
@@ -25,10 +28,13 @@ const app = Vue.createApp({
                 this.picture = results[0].picture.large,
                     this.name = results[0].name.first + " " + results[0].name.last,
                     this.gender = results[0].gender,
-                    this.btntxt = "Flush";
-                    this.check = "null"
+                    this.btntxt = "Flush",
+                    this.check = "null";
                 if (this.gender == "male") this.shadow = "blue"
-                else if (this.gender == "female") this.shadow = "pink"
+                else if (this.gender == "female") this.shadow = "pink";
+                    this.twitter = "https://twitter.com/"+this.username,
+                    this.snapchat = "https://snapchat.com/"+this.username,
+                    this.instagram = "https://www.instagram.com/"+this.username;
             }, 500)
         },
     }
