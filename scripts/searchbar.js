cabe5f6362edd78ecf1800cc6e1f8a3b0e4d17d0
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2022-11-26 13:14:43 +0530

    Added Search bar animation

--- scripts/searchbar.js
+++ scripts/searchbar.js
@@ -1,6 +1,11 @@
-$(document).ready(function(){
-  $('#lookup').click(function(){
-    $('#search').css({'opacity':'100%','z-index':'8'});
+$(document).ready(function () {
+  $('#lookup').click(function () {
+    $('#search').css({ 'opacity': '100%', 'z-index': '8' });
     $('input').focus();
+    if (window.matchMedia("(max-width: 767px)").matches) $('#nav').click();
+  });
+  $('#search-submit').click(function () {
+    $('#search').css({ 'opacity': '0', 'z-index': '-1', 'transition': 'all 0.5s ease-in-out' });
+
   });
 });
