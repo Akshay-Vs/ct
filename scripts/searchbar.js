d94bfe6b8a07a09dbb9140fb1d9f936f4afaddd1
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2022-11-26 12:30:22 +0530

    Added Search bar animation

--- scripts/searchbar.js
+++ scripts/searchbar.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#lookup').click(function(){
-    $('#search').css({'opacity':'100%','z-index':'10'});
-    $('#overlay').css({'opacity':'100%','z-index':'10'});
+    $('#search').css({'opacity':'100%','z-index':'8'});
+    $('input').focus();
   });
 });
